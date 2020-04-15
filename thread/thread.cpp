65a03567681a29d770680daf486dd0bbd233beda
kenya <k.shinki0218@gmail.com>
2020-04-15 22:15:13 +0900

    add

--- thread/thread.cpp
+++ thread/thread.cpp
@@ -5,10 +5,14 @@ int main()
 {
     int x = 0, y = 0;
 
-    std::thread t([&]{ ++x; });
+    std::thread t1([&]{ ++x; });
+    std::thread t2([&]{ ++x; });
     --y;
-    t.join();
+    t1.join();
+    t2.join();
 
     std::cout << x << " " << y << std::endl;
+    std::cout << t1.get_id() << std::endl;
+    std::cout << t2.get_id() << std::endl;
     return 0;
 }
